c5d6fbe7b67c96b24a9435985ef8ffd32e5992a2
Santosh Shah <santoshofficial679@gmail.com>
2024-02-09 01:28:01 +0545

    Made some updates to the ATM implementation

---
+++ src/main/java/com/progressoft/induction/atm/Impl/ATMImpl.java
@@ -0,0 +1,34 @@
+package com.progressoft.induction.atm.Impl;
+
+import com.progressoft.induction.atm.ATM;
+import com.progressoft.induction.atm.Banknote;
+import com.progressoft.induction.atm.exceptions.AccountNotFoundException;
+import com.progressoft.induction.atm.exceptions.InsufficientFundsException;
+import com.progressoft.induction.atm.exceptions.NotEnoughMoneyInATMException;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ATMImpl implements ATM {
+    private final BankingSystemImpl bankingSystem = new BankingSystemImpl();
+    @Override
+    public List<Banknote> withdraw(String accountNumber, BigDecimal amount) {
+        // Your code here
+        BigDecimal accountBalance =   checkBalance(accountNumber);
+
+        if(accountBalance.compareTo(amount) < 0)
+        {
+            throw new InsufficientFundsException("Insufficient balance.");
+        }
+
+        bankingSystem.debitAccount(accountNumber,amount);
+
+        return null;
+    }
+
+    @Override
+    public BigDecimal checkBalance(String accountNumber) {
+        return bankingSystem.getAccountBalance(accountNumber);
+    }
+}
